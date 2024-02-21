df4b462ce838c233c0dfe65a95e983f68e03c060
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-02-22 01:10:33 +0500

    results fetched in order and on the basis of acc

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
-import { getFirestore,collection, addDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getFirestore, collection, query, where, getDocs, addDoc,orderBy, limit } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 const firebaseConfig = {
     apiKey: "AIzaSyCV86XxNzlEx4YFPxuo396uJVLRODoJFs0",
     authDomain: "typing-results.firebaseapp.com",
@@ -14,17 +14,25 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
-document.getElementById("typing").addEventListener("input",async()=>{
-    if(document.getElementById("typing").value.length == localStorage.getItem("length")){
+document.getElementById("typing").addEventListener("input", async () => {
+    if (document.getElementById("typing").value.length == localStorage.getItem("length")) {
         try {
-            const docRef = await addDoc(collection(db, "users"), {
-                typer: localStorage.getItem("name"),
-                wpm: localStorage.getItem("type"),
-                acc:localStorage.getItem("accuracy")
-            });
-            console.log("Document written with ID: ", docRef.id);
+            if(localStorage.getItem("accuracy")>=45){    
+                const docRef = await addDoc(collection(db, "users"), {
+                    typer: localStorage.getItem("name"),
+                    wpm: Number(localStorage.getItem("type")),
+                    acc: Number(localStorage.getItem("accuracy"))
+                });
+                console.log("Document written with ID: ", docRef.id);
+            }
         } catch (e) {
             console.error("Error adding document: ", e);
         }
     }
-    })
+})
+const q = query(collection(db, "users"), orderBy("wpm","desc"), limit(10));
+
+const querySnapshot = await getDocs(q);
+querySnapshot.forEach((doc) => {
+  console.log(doc.id, " => ", doc.data());
+});
